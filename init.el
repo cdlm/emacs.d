dcbc8ddf1c1f22d9194cebf2c73832966ff4017c
Damien Pollet <damien.pollet@gmail.com>
2014-09-13 02:07:24 +0200

    Pass over packages and their configuration.

--- init.el
+++ init.el
@@ -6,98 +6,164 @@
 (require 'pallet)
 (require 'use-package)
 
-(require 'powerline)
-(powerline-default-theme)
-
-(use-package drag-stuff
-  :diminish drag-stuff-mode
-  :init (progn
-	  (setq drag-stuff-modifier '(super control))
-	  (add-to-list 'drag-stuff-except-modes 'org-mode)
-	  (add-to-list 'drag-stuff-except-modes 'rebase-mode)
-	  (drag-stuff-global-mode t)))
-
-(use-package magit
-  :bind ("C-x g" . magit-status)
-  :config (progn
-	    (use-package magit-svn
-	      :diminish magit-svn-mode)
-	    (use-package magit-gh-pulls)
-	    (add-hook 'magit-mode-hook 'turn-on-magit-svn)
-	    (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)))
-
-(use-package ido
-  :config (progn
-	    (use-package flx-ido
-	      :init (progn
-		      (ido-mode 1)
-		      (ido-everywhere 1)
-		      (flx-ido-mode 1)
-		      ;; disable ido faces to see flx highlights.
-		      (setq ido-use-faces t)))
-	    (use-package ido-vertical-mode
-	      :init (ido-vertical-mode 1))
-	    (use-package ido-ubiquitous
-	      :init (ido-ubiquitous-mode)))
-  :init (ido-mode 'buffer))
-
-(use-package smex
-  :bind ("M-x" . smex))
-
-(use-package recentf
-  :defer t
-  :config (use-package recentf-ext))
-
-
-(use-package paren-face
-  :init (progn
-	  (global-paren-face-mode)))
-
-(use-package rainbow-delimiters
-  :config (progn
-	    (show-paren-mode)
-	    (show-smartparens-mode))
-  :commands rainbow-delimiters-mode)
-
-(use-package smartparens
-  ;; additional pairs
-  :diminish smartparens-mode)
-
+(use-package bind-key)
 
 (use-package color-theme-sanityinc-solarized
   :if (display-graphic-p)
   :config (progn
-      (load-theme 'sanityinc-solarized-dark)
-      (set-face-background 'cursor "#df4") ; I like it to flash
-      (color-theme-sanityinc-solarized--with-colors
-       'dark
-       (set-face-foreground 'parenthesis faintest))))
+            (use-package paren-face
+              ;; dimmer parentheses
+              :config (global-paren-face-mode))
+            (load-theme 'sanityinc-solarized-dark)
+            (set-face-background 'cursor "#df4") ; I like it to flash
+            (color-theme-sanityinc-solarized--with-colors
+             'dark
+             (set-face-foreground 'parenthesis faintest))))
+
+(use-package powerline
+  ;; colorful modeline
+  :config (progn
+            (use-package anzu ;; number of search matches
+              :diminish anzu-mode
+              :config (global-anzu-mode))
+            (powerline-default-theme)))
+
+(use-package evil
+  :commands evil-mode
+  :config (progn
+            (use-package evil-leader)
+            (use-package evil-matchit)
+            (use-package evil-paredit)
+            (use-package evil-surround)))
+
+(use-package drag-stuff
+  :diminish drag-stuff-mode
+  :idle (progn
+          (setq drag-stuff-modifier '(super control))
+          (add-to-list 'drag-stuff-except-modes 'org-mode)
+          (add-to-list 'drag-stuff-except-modes 'rebase-mode)
+          (drag-stuff-global-mode)))
+
+(use-package expand-region
+  ;; select around cursor, C-- C-= to contract
+  :bind ("C-=" . er/expand-region))
+
+(use-package yasnippet
+  :idle (progn
+          (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
+          (yas-global-mode)))
+
+(use-package smart-tab
+  :diminish smart-tab-mode
+  :idle (progn
+          (global-smart-tab-mode)
+          (add-to-list 'smart-tab-disabled-major-modes 'help-mode)
+          (add-to-list 'smart-tab-disabled-major-modes 'Custom-mode)))
+
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  :idle (progn
+                (use-package magit-svn
+                  :diminish magit-svn-mode)
+                (use-package magit-gh-pulls)
+                (add-hook 'magit-mode-hook 'turn-on-magit-svn)
+                (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)))
+
+(use-package git-gutter
+  ;; show diff hunks in gutter + stage/unstage from buffer
+  :diminish git-gutter-mode
+  :idle (global-git-gutter-mode)
+  :config (progn
+            (bind-keys
+             ("C-x C-g C-n" . git-gutter:next-hunk)
+             ("C-x C-g C-p" . git-gutter:previous-hunk)
+             ("C-x C-g C-s" . git-gutter:stage-hunk)
+             ("C-x C-g C-r" . git-gutter:revert-hunk))))
+
+(use-package ido
+  :config (progn
+                (use-package flx-ido
+                  :init (progn
+                          (ido-mode 1)
+                          (ido-everywhere)
+                          (flx-ido-mode 1)
+                          (setq ido-enable-flex-matching t)
+                          (setq ido-use-faces t)))
+                (use-package ido-vertical-mode
+                  :init (ido-vertical-mode))
+                (use-package ido-ubiquitous
+                  :init (ido-ubiquitous-mode)))
+  :init (ido-mode 'buffer))
+
+(use-package smex
+  ;; M-x interface with colors and completion
+  :bind ("M-x" . smex))
+
+(use-package recentf
+  ;; recent files
+  :config (use-package recentf-ext))
 
 
-(use-package auctex-latexmk
-  :config (auctex-latexmk-setup))
+(use-package hl-line
+  ;; visible current line
+  :config (global-hl-line-mode))
 
+(use-package ethan-wspace) ;; intelligent showing / autocleaning of whitespace
+
+(use-package highlight-parentheses
+  :diminish highlight-parentheses-mode
+  :config (show-paren-mode)
+  :commands highlight-parentheses-mode)
+
+;; (use-package smartparens :diminish smartparens-mode)
+(use-package autopair
+  :diminish autopair-mode
+  :idle (autopair-mode))
+
+(use-package browse-url
+  :bind ("s-<mouse-1>" . browse-url-at-mouse))
+
+
+(use-package tex-site ;; auctex
+  :idle
+  :config (progn
+            (use-package auctex-latexmk
+              :config (auctex-latexmk-setup))))
+
+(use-package pillar
+  :mode ("\\.\\(pier\\|pillar\\)\\'" . pillar-mode))
+
+(use-package markdown-mode
+  :mode ("\\.\\(md\\|markdown\\)\\'" . markdown-mode)
+  :config (progn
+            (use-package pandoc-mode
+              :config (add-hook 'markdown-mode-hook 'turn-on-pandoc))))
+
+(use-package rbenv
+  :config (global-rbenv-mode))
 
 (defun add-to-executable-path (path)
   (let ((expanded-path (expand-file-name path)))
-    (add-to-list 'exec-path expanded-path)
-    (setenv "PATH" (concat expanded-path ":" (getenv "PATH")))))
+        (add-to-list 'exec-path expanded-path)
+        (setenv "PATH" (concat expanded-path ":" (getenv "PATH")))))
 
-(when (display-graphic-p)		; GUI settings
-  (cd "~")				; OS X apps launch from /
-  (mapc 'add-to-executable-path		; and do not inherit the user's shell environment
-	(list
-	 "/opt/texlive/2014/bin/x86_64-darwin"
-	 "/opt/homebrew/bin"))
+
+(when (display-graphic-p)               ; GUI settings
+  (cd "~")                              ; OS X apps launch from /
+  (mapc 'add-to-executable-path                 ; and do not inherit the user's shell environment
+        (list
+         "/opt/texlive/2014/bin/x86_64-darwin"
+         "/opt/homebrew/bin"))
   (add-hook 'after-init-hook 'server-start))
 
-(when (not (display-graphic-p))		; terminal settings
+
+(when (not (display-graphic-p))                 ; terminal settings
   (menu-bar-mode -1)
-  ; (load-theme 'wombat)			; unnecessary it seems
+  ; (load-theme 'wombat)                        ; unnecessary it seems
 )
 
 ;; Make all "yes or no" prompts show "y or n" instead
 (fset 'yes-or-no-p 'y-or-n-p)
-
 (delete-selection-mode)
-; browse-url-at-mouse
+(setq-default indent-tabs-mode nil)
+(bind-key "s-/" 'comment-line)
